3b02251700e28e5798448968e4baf9a1e97a488b
Calum Heldt <cal.muon32@gmail.com>
2026-04-26 09:30:06 -0500

    test period 100

--- simulate/bifurcations/bifurcations_3D.py
+++ simulate/bifurcations/bifurcations_3D.py
@@ -35,8 +35,9 @@ food = 170              # OKAY # number of bats it would take to deplete food co
 
 time = 3650             # total days
 winter = 120            # CONFIDENT # length of winter season in Nebraska mines
-recovery_period = 60    # number of days spent in recovery before re-infection is possible
+recovery_period = 100    # number of days spent in recovery before re-infection is possible
 
+# last tested 60
 # ------------------
 # bifurcation values
 # ------------------
